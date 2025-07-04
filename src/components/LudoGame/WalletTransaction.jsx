893b8ad8133f1a9b3fe52c13e63640e878fc05b2
Muhammad Waqas <waqasshahab1999@gmail.com>
2025-07-04 23:18:30 +0500

    fix

--- src/components/LudoGame/WalletTransaction.jsx
+++ src/components/LudoGame/WalletTransaction.jsx
@@ -16,18 +16,6 @@ export default function WalletTransaction() {
       ) : (
         <WithdrawScreen onSwitchToDeposit={() => setActiveTab("deposit")} />
       )}
-
-      {/* Bottom Browser Bar */}
-      <div className="fixed bottom-0 left-0 right-0 bg-gray-800/90 backdrop-blur-sm p-4">
-        <div className="bg-gray-700 rounded-full px-4 py-2 flex items-center justify-center">
-          <span className="text-gray-300 text-sm">bc.game</span>
-        </div>
-      </div>
-
-      {/* Bottom indicator */}
-      <div className="fixed bottom-2 left-1/2 -translate-x-1/2">
-        <div className="w-32 h-1 bg-gray-600 rounded-full"></div>
-      </div>
     </div>
     </div>
   )
